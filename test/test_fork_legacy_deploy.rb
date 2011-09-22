32d92115d4886fd31797e0fdda6d335acc6cc80f
Sam Tubbax <sam@sumocoders.be>
2011-09-22 14:19:52 +0200

    Initial commit to fork_legacy_deploy.

---
+++ test/test_fork_legacy_deploy.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestForkLegacyDeploy < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
