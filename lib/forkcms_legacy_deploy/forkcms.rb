d6de62f70cf62989f8b758ddc86ce85100ebe9dd
sam@sumocoders.be <sam@samtubbax.be>
2011-10-21 14:42:51 +0200

    changed for supershop purposes

--- lib/forkcms_legacy_deploy/forkcms.rb
+++ lib/forkcms_legacy_deploy/forkcms.rb
@@ -31,27 +31,27 @@ configuration.load do
 		desc 'link the document root to the current/default_www-folder'
 		task :link_document_root do
 			# create symlink for document_root if it doesn't exists
-			documentRootExists = capture("if [ ! -e #{document_root} ]; then ln -sf #{current_path}/public #{document_root}; echo 'no'; fi").chomp
+			documentRootExists = capture("if [ ! -e #{document_root} ]; then ln -sf #{current_path}/default_www #{document_root}; echo 'no'; fi").chomp
 
 			unless documentRootExists == 'no'
 				warn "Warning: Document root (#{document_root}) already exists"
 				warn "to link it to the Fork deploy issue the following command:"
-				warn "	ln -sf #{current_path}/public #{document_root}"
+				warn "	ln -sf #{current_path}/default_www #{document_root}"
 			end 
 		end	
 
 		desc 'Create needed symlinks'
 		task :link_files do
 			# get the list of folders in /frontend/files
-			folders = capture("ls -1 #{release_path}/public/userfiles").split(/\r?\n/)
+			folders = capture("ls -1 #{release_path}/default_www/userfiles").split(/\r?\n/)
 
 			# loop the folders
 			folders.each do |folder|
 				# copy them to the shared path, remove them from the release and symlink them
 				run %{
-					cp -r #{release_path}/public/userfiles/#{folder} #{shared_path}/files/#{folder} &&
-					rm -rf #{release_path}/public/userfiles/#{folder} &&
-					ln -s #{shared_path}/files/#{folder} #{release_path}/public/userfiles/#{folder}
+					cp -r #{release_path}/default_www/userfiles/#{folder} #{shared_path}/files/#{folder} &&
+					rm -rf #{release_path}/default_www/userfiles/#{folder} &&
+					ln -s #{shared_path}/files/#{folder} #{release_path}/default_www/userfiles/#{folder}
 				}
 			end
 		end	
